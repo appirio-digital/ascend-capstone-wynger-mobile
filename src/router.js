4a32fec7a080aa2b7fe013175523a82e0886131a
charlieroth <roth47@purdue.edu>
2019-07-22 10:47:37 -0400

    Added tabbar icons

--- src/router.js
+++ src/router.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Icon } from 'native-base'
 
 import ProductsScreen from './screens/Products';
 import AccountsScreen from './screens/Accounts';
@@ -7,10 +9,30 @@ import AuthenticateScreen from './screens/Authenticate';
 import HomeScreen from './screens/Home';
 
 export const TabNavigator = createBottomTabNavigator({
-    Home: HomeScreen,
-    Products: ProductsScreen,
-    Accounts: AccountsScreen,
-    Barcode: BarcodeScannerScreen,
-});
+  Home: HomeScreen,
+  Products: ProductsScreen,
+  Accounts: AccountsScreen,
+  Barcode: BarcodeScannerScreen,
+}, {
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, horizontal, tintColor }) => {
+      const { routeName } = navigation.state;
+      
+      let iconName = '';
+      if (routeName === 'Products') {
+        iconName = 'ios-pricetags';
+      } else if (routeName === 'Accounts') {
+        iconName = 'ios-people';
+      } else if (routeName === 'Barcode') {
+        iconName = 'ios-qr-scanner';
+      } else {
+        iconName = 'ios-home';
+      }
+
+      // You can return any component that you like here!
+      return <Icon type="Ionicons" name={iconName} />;
+    }
+  }),
+})
 
 export default createAppContainer(TabNavigator);
